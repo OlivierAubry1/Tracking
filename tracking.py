dd06b513d77fcda3a243d1731780eb90bb3cacf7
olivier aubr <aubo1502@usherbrooke.ca>
2025-11-23 17:24:53 -0500

    mediapipe integration

--- tracking.py
+++ tracking.py
@@ -1,4 +1,9 @@
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(min_detection_confidence=0.7)
 
 webcam = cv2.VideoCapture(0)
 while True:
@@ -6,6 +11,11 @@ while True:
     if success == False:
         break
     frame = cv2.flip(frame, 1)
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(rgb_frame)
+    
     cv2.imshow("Counting finger",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
