36b1b4a4f87e7a8e30ce60c05588d3ebe14909d0
olivier aubr <aubo1502@usherbrooke.ca>
2025-11-23 17:08:54 -0500

    fixed app crashing by adding quit button

--- tracking.py
+++ tracking.py
@@ -2,10 +2,12 @@ import cv2
 
 webcam = cv2.VideoCapture(0)
 while True:
-    success, feed = webcam.read()
+    success, frame = webcam.read()
     if success == False:
         break
     frame = cv2.flip(frame, 1)
     cv2.imshow("Counting finger",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 webcam.release()
 cv2.destroyAllWindows()
