fcd4a5a8ce67862a2d3b8772d7ef943dd8547847
olivier aubr <aubo1502@usherbrooke.ca>
2025-11-23 17:27:29 -0500

    drawing

--- tracking.py
+++ tracking.py
@@ -15,6 +15,10 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp.drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HANDS_CONNECTIONS)
     
     cv2.imshow("Counting finger",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
